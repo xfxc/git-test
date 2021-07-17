475afdb93702ac4a54b441a6a31548ec135c3ae5
xc <xfxc88@126.com>
2021-07-17 21:14:05 +0800

    idea push

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix line");
         System.out.println("master added line");
         System.out.println("hot-fix added line");
+        System.out.println("idea push");
     }
 }
