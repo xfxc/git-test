3710ca027c67688717ba89624b24e5a4a7de23ad
xc <xfxc88@126.com>
2021-07-17 20:37:27 +0800

    master conflict commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("second commit");
         System.out.println("xc third commit");
         System.out.println("hot-fix line");
+        System.out.println("master added line");
     }
 }
