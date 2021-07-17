b82add1ed2e7cbb499f772bdc6b337b517a45125
xc <xfxc88@126.com>
2021-07-17 20:24:59 +0800

    second commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -3,5 +3,6 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test");
+        System.out.println("second commit");
     }
 }
