38453787a2743cab625a979cb6a299381c1632a3
xc <xfxc88@126.com>
2021-07-17 20:25:35 +0800

    third commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("second commit");
+        System.out.println("xc third commit");
     }
 }
